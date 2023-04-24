98a1a8a198a62b39829a71ad7eb42177774bf1a9
Sender60 <butkov60@gmail.com>
2023-04-24 21:38:06 +0300

    Исправил опечатку

--- project/src/utils.ts
+++ project/src/utils.ts
@@ -2,7 +2,7 @@ export default function passValidate(pas:string){
   if(pas.search(/[A-Za-z]/) === -1){
     return false;
   }
-  if(pas.search(/[0-1]/) === -1){
+  if(pas.search(/[0-9]/) === -1){
     return false;
   }
   return true;
